16c391b7deecdd27c869ccad7daf6f983717bea7
MaBadiDen <kaba4ok.den@gmail.com>
2023-02-15 03:12:02 +0300

    ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] parts = stringSplit("Cats were here!!!!", 3);
+
+        for (String part : parts) {
+            System.out.println(part);
+        }
+
     }
+    public static String[] stringSplit(String str, int partsCount) {
+        if(str == null || partsCount == 0) {
+            throw new RuntimeException();
+        }
+        int length = str.length();
+        int partSize = length/partsCount;
+        int tailString = length % partsCount;
+        String[] parts = new String[partsCount];
+
+        for (int i = 0; i < partsCount; i++) {
+            int start = i*partSize;
+            int end = start + partSize;
+            if(i == partsCount-1) end += tailString;
+            parts[i] = str.substring(start, end);
+
+        }
+
+        return parts;
+    }
+
 }
